b0378162532323ca85caee589c69d843eb71d9f9
Jonah Christian <jonahchrist10@gmail.com>
2021-11-09 12:21:16 -0500

    added more validation to new reservation form

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -18,11 +18,32 @@ function hasRequiredFields(req, res, next) {
     }
   });
   if (missingFields.length === 0) {
+    res.locals.reservation = req.body.data;
     return next();
   }
   next({
     status: 400,
     message: `Body is missing the following properties: ${missingFields.join(", ")}`
+  });
+}
+
+function validDate(req, res, next) {
+  const errors = [];
+  const reservation = res.locals.reservation;
+  const date = new Date(`${reservation.reservation_date}T${reservation.reservation_time}`);
+  const today = new Date();
+  if (date.getUTCDay() === 2) {
+    errors.push("The restaurant is closed on Tuesdays.");
+  }
+  if (date - today < 0) {
+    errors.push("Cannot book a past date.")
+  }
+  if (errors.length === 0) {
+    return next();
+  }
+  next({
+    status: 400,
+    message: `${errors.join(" ")} Please pick a different day.`
   });
 }
 
@@ -33,12 +54,12 @@ async function list(req, res) {
 }
 
 async function create(req, res) {
-  const newReservation = req.body.data;
+  const newReservation = res.locals.reservation;
   const data = await service.create(newReservation);
   res.status(201).json({ data });
 }
 
 module.exports = {
   list: asyncErrorBoundary(list),
-  create: [hasRequiredFields, asyncErrorBoundary(create)],
+  create: [hasRequiredFields, validDate, asyncErrorBoundary(create)],
 };
